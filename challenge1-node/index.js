437fcf3dc33244875717bf43be9f36006e21c202
Neto6391 <joseweedodev@gmail.com>
2025-09-26 19:58:26 -0300

    feat: refactor basic server to use express

--- challenge1-node/index.js
+++ challenge1-node/index.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
 
-const http = require("http");
+const express = require("express");
+const app = express();
 
 const hostname = process.env.HOSTNAME;
 const port = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!\n");
+app.get("/", (req, res) => {
+  res.send("Hello, World!\n");
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
